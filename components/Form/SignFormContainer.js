5388eba00f34bd3f707fc2aedd3438e772df08c9
Anton Kjörck Lindén <anton.kjorck.linden@marknadsinformation.se>
2019-01-27 16:02:59 +0100

    Removes firebase support

--- components/Form/SignFormContainer.js
+++ components/Form/SignFormContainer.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import Form from './Form'
 import { reduxForm } from 'redux-form';
-import firestore from "../../firebase.config";
 
 export const SignFormContainer = ({ handleSubmit }) => {
   const submitForm = (formValues) => {
     console.log('submitting Form: ', formValues);
-    const db = firestore.firestore();
-    const signsRef = db.collection('signs').add({
-      firstName: formValues.firstName,
-      lastName: formValues.lastName,
-      email: formValues.email,
-      country: formValues.country
-    }); 
   }
 
   return (
