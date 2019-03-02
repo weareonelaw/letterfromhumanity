2d8b943ae8e88eabc23b3966bd24d483786d21e6
Anton Hedström <antonhedstrom@gmail.com>
2019-03-02 22:03:34 +0100

    Added Hamburger menu.

--- components/Menu/MainMenu.js
+++ components/Menu/MainMenu.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions/creators';
+import './MainMenu.module.sass';
 
-const MainMenu = ({menuOpen}) => {
-  if(menuOpen) return (
-    <div>MAIN MENU VISIBLE</div>
-  )
-  else return null;
+const MainMenu = ({ toggleMainMenu, menuOpen }) => {
+  return (
+    <div className={`main-menu ${menuOpen ? 'open' : ''}`}>
+      <div className="overlay" onClick={toggleMainMenu}></div>
+      <div className="menu-container">
+        <ul className="links">
+          <li className="link"><a href="#sign">Sign</a></li>
+          <li className="link"><a href="#about">About</a></li>
+          <li className="link"><a href="#social">Social</a></li>
+          <li className="link"><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default connect(
   state => ({
     menuOpen: state.ui.menuOpen
   }),
-  null
-)(MainMenu)
+  actions,
+)(MainMenu);
--- components/Menu/ToggleButton.js
+++ components/Menu/ToggleButton.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import * as actions from '../../store/actions/creators';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import './ToggleButton.module.sass';
 
 const ToggleButton = ({toggleMainMenu, menuOpen}) => {
   const menuText = (menuOpen) ? "Close menu"  : "Open menu";
-  return <button onClick={toggleMainMenu}>{menuText}</button>;
+  return (
+    <div className={`menu-toggler container ${menuOpen ? 'opened' : ''}`} onClick={toggleMainMenu}>
+      <div class="bar1"></div>
+      <div class="bar2"></div>
+      <div class="bar3"></div>
+    </div>
+  );
+  return <button className="menu-toggler" onClick={toggleMainMenu}>{menuText}</button>;
 };
 
 export default connect(
