2dbd1dd3a7011ec3ce4360ab1fafa6806aa93f8a
Anton Kjörck Lindén <anton.kjorck.linden@marknadsinformation.se>
2019-03-11 19:00:22 +0100

    Add inital props to progress container

--- components/ProgressBar/ProgressContainer.js
+++ components/ProgressBar/ProgressContainer.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import ProgressBar from './ProgressBar';
 import * as actions from "../../store/actions/creators";
 
-class ProgressContainer extends React.Component {
+class ProgressContainer extends Component {
   componentDidMount() {
     this.props.getProgress();
   }
+  static getInitialProps({store, isServer, pathname, query}) {
+}
   render() {
     const { progressData } = this.props;
-    return <ProgressBar progressData={progressData} />;
+    return <ProgressBar progressData={progressData} />
   }
 };
 
