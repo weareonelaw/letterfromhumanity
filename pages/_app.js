e9edd438a593a00d17e5b08350c97ebaf3d9b7ea
Anton Hedström <antonhedstrom@gmail.com>
2019-04-04 17:56:57 +0200

    Added Material UI and styled form.

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,28 @@ import '../sass/application.scss';
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '../store/configurateStore';
-import App, {Container} from "next/app";
-import withRedux from "next-redux-wrapper";
+import JssProvider from 'react-jss/lib/JssProvider';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import getPageContext from '../getPageContext';
+import App, {Container} from 'next/app';
+import Head from 'next/head';
+import withRedux from 'next-redux-wrapper';
 
 class MyApp extends App {
+  constructor() {
+    super();
+    this.pageContext = getPageContext();
+  }
+
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
   static async getInitialProps({Component, ctx}) {
     const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
     return {pageProps};
@@ -14,9 +32,37 @@ class MyApp extends App {
     const {Component, pageProps, store} = this.props;
     return (
       <Container>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
+        <Head>
+          <title>Onelaw</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta
+            name="theme-color"
+            content={this.pageContext ? this.pageContext.theme.palette.primary.main : null}
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
+        </Head>
+        <JssProvider
+          registry={this.pageContext.sheetsRegistry}
+          generateClassName={this.pageContext.generateClassName}
+        >
+          {/* MuiThemeProvider makes the theme available down the React
+              tree thanks to React context. */}
+          <MuiThemeProvider
+            theme={this.pageContext.theme}
+            sheetsManager={this.pageContext.sheetsManager}
+          >
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            {/* Pass pageContext to the _document though the renderPage enhancer
+                to render collected styles on server-side. */}
+            <Provider store={store}>
+              <Component pageContext={this.pageContext} {...pageProps} />
+            </Provider>
+          </MuiThemeProvider>
+        </JssProvider>
       </Container>
     )
   }
