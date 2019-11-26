3175d5956cb997dac327612b9b2be6ece3bdf285
Anton Kjörck Lindén <anton.kjorck.linden@marknadsinformation.se>
2019-11-26 19:34:50 +0100

    Fixes validation

--- components/SignForm/validate.js
+++ components/SignForm/validate.js
@@ -1,18 +1,18 @@
 const validate = values => {
   const errors = {}
 
-  if (!values.firstName) {
-    errors.firstName = 'Required field'
+  if (!values.firstname) {
+    errors.firstname = 'Required field'
   }
-  if (!values.lastName) {
-    errors.lastName = 'Required field'
+  if (!values.lastname) {
+    errors.lastname = 'Required field'
   }
   if (!values.email) {
     errors.email = 'Required field'
   }
   if (!values.agree) {
     errors.agree = 'You need to check the box to sign the letter :-)'
-  } 
+  }   
 
   return errors
 }
